6ec1ed8981584ff7586a5298e1bfd18688b8800b
Filip Skubacz <filip.skubacz@tum.de>
2025-09-20 20:19:58 +0200

    Yaw + rel pos obs with CL

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
@@ -127,8 +127,8 @@ class AMPUnitreeGo2StairsEnvCfg(LocomotionVelocityRoughEnvCfg):
         parameters.set_stairs_env_cfg_cmds(self)
         parameters.set_stairs_env_cfg_reset_base(self)
         parameters.add_relative_position_on_stairs_observation(self)
-        self.observations.policy.yaw = None
-        self.observations.policy.relative_position = None
+        # self.observations.policy.yaw = None
+        # self.observations.policy.relative_position = None
         parameters.add_stair_parameters_observation(self)
 
         parameters.set_velocity_rewards_amp(self)
