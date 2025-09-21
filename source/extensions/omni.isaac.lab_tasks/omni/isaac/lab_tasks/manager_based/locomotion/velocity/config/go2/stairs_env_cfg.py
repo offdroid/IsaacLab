2dfbf79abcdee9d86e121373430bce134d205d54
Filip Skubacz <filip.skubacz@tum.de>
2025-09-21 17:50:06 +0200

    DS Slow x2

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
@@ -240,7 +240,7 @@ class AMPUnitreeGo2StairsEnvCfg(LocomotionVelocityRoughEnvCfg):
         parameters.set_amp_settings(self, use_rsi=False)
         # update motion files
         self.amp_motion_folder = (
-            "datasets/fromVision_motions_DepthCam_stairs_feetZAmpl_minimal_slow/*"
+            "datasets/fromVision_motions_DepthCam_stairs_feetZAmpl_minimal_slowx2/*"
         )
         self.amp_motion_files = glob.glob(self.amp_motion_folder)
 
