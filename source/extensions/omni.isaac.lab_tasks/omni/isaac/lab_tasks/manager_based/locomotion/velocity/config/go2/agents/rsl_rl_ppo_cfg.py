dade60db365039edda70eff60abcfec50a63e729
Filip Skubacz <filip.skubacz@tum.de>
2025-09-24 18:25:41 +0200

    Task reward lerp = 0.625

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/agents/rsl_rl_ppo_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/agents/rsl_rl_ppo_cfg.py
@@ -72,7 +72,7 @@ class UnitreeGo2AMPFlatPPORunnerCfg(UnitreeGo2FlatPPORunnerCfg):
         self.amp_motion_folder = 'datasets/dummy/*'
         self.amp_motion_files = glob.glob(self.amp_motion_folder)
         self.amp_num_preload_transitions = 2_000_000
-        self.amp_task_reward_lerp = 0.75 # weighting factor of task reward (style reward is 1-task_reward_lerp)
+        self.amp_task_reward_lerp = 0.625 # weighting factor of task reward (style reward is 1-task_reward_lerp)
         self.amp_discr_hidden_dims = [1024, 512]
 
         self.min_normalized_std = [0.05] * 4 + [0.02] * 4 +[0.05] * 4#  + [0.05] # for ResidualRL
