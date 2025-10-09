f5ae2f61045b03d2f073a9ec26545fa93d9843b1
Filip Skubacz <filip.skubacz@tum.de>
2025-10-09 13:03:52 +0200

    Fix terrain levels

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
@@ -297,10 +297,7 @@ class AMPUnitreeGo2ShortStairsEnvCfg(AMPUnitreeGo2StairsEnvCfg):
             "yaw": (-0.5, 0.5),
         }
         self.events.push_robot.interval_range_s = (1.0, 5.0)
-        self.curriculum.terrain_levels = CurrTerm(
-            func=mdp.terrain_levels_vel,
-            params={"custom_required_distance_for_move_up": 0.2 * 8},
-        )
+        self.curriculum.terrain_levels.params["custom_required_distance_for_move_up"] = 0.3 * 8 * 3 / 4
 
 @configclass
 class AMPUnitreeGo2ShortStairsEnvCfg_PLAY(AMPUnitreeGo2ShortStairsEnvCfg):
