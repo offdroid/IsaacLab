31bcf4385b6eaa13df68b1c83f3f1fd5dee952f2
Filip Skubacz <filip.skubacz@tum.de>
2025-10-13 16:54:52 +0200

    Remove scheduled rewards for basic

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
@@ -582,6 +582,11 @@ class AMPUnitreeGo2ShortStairsBasicEnvCfg(AMPUnitreeGo2StairsEnvCfg):
         parameters.set_terrain(self)
         parameters.set_rewards_simple(self)
 
+        for field in fields(cfg.curriculum):
+            if field.name == "terrain_levels":
+                continue
+            setattr(cfg.curriculum, field.name, None)
+
         self.episode_length_s = 8.0
 
         # Random force pushes on body
