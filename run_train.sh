3aa4832b3f2ea4fc931ce1fc3afcf75dc7c8613c
Filip Skubacz <filip.skubacz@tum.de>
2025-10-25 10:14:26 +0200

    Add DSv3 + DSv5 dataset again

--- run_train.sh
+++ run_train.sh
@@ -11,5 +11,5 @@ fi
 
 seeds=(3)
 for seed in "${seeds[@]}"; do
-  ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-AMPShortStairs-Unitree-Go2-v0 --headless --seed $seed --log_dir "${datetime}_amp_DRv6_DSv5_wildcard4_lessdiversestairs_lesstorquepen" --logger wandb --log_project_name amp_6dr --max_iterations 20000 --num_envs 4096
+  ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-AMPShortStairs-Unitree-Go2-v0 --headless --seed $seed --log_dir "${datetime}_amp_DRv6_DSv3_DSv5_wildcard4_lessdiversestairs_lesstorquepen" --logger wandb --log_project_name amp_6dr --max_iterations 20000 --num_envs 4096
 done
