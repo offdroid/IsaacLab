45773fbbea2582c7adbb920fe00f5a830835ddef
Filip Skubacz <filip.skubacz@tum.de>
2025-11-01 20:05:07 +0100

    v3 + v5 DS but only flat walking data. Untested.

--- run_train.sh
+++ run_train.sh
@@ -12,6 +12,6 @@ fi
 seeds=(1 2 3)
 # seeds=(1 2 3)
 for seed in "${seeds[@]}"; do
-  ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-AMPShortStairs-Unitree-Go2-v0 --headless --seed $seed --log_dir "${datetime}_amp_basic_mocap_final" --logger wandb --log_project_name amp_mocap_final --max_iterations 20000 --num_envs 4096
+  ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-AMPShortStairs-Unitree-Go2-v0 --headless --seed $seed --log_dir "${datetime}_amp_basic_v3_v5_onlywalk" --logger wandb --log_project_name amp_v3_v5_onlywalk_final --max_iterations 20000 --num_envs 4096
   # ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-Stairs-ComplexReward-Unitree-Go2-v0 --headless --seed $seed --log_dir "${datetime}_complex" --logger wandb --log_project_name complex_v2 --max_iterations 10000 --num_envs 4096
 done
--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
@@ -483,7 +483,7 @@ class AMPUnitreeGo2StairsEnvCfg(LocomotionVelocityRoughEnvCfg):
 
         parameters.set_amp_settings(self, use_rsi=False)
         # update motion files
-        self.amp_motion_folder = "datasets/mocap_AMP_for_hardware_reduced/*"
+        self.amp_motion_folder = "datasets/fromVision_motions_DepthCam_stairsv3+v5fast_only_walk_feetZAmpl_minimal/*"
         self.amp_motion_files = glob.glob(self.amp_motion_folder)
 
     def update_motion_files(self):
