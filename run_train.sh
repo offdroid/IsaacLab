91cc8d068fe9604bbb664ff123c62088cc143231
Filip Skubacz <filip.skubacz@tum.de>
2025-11-02 10:36:27 +0100

    Task reward = 0.25

--- run_train.sh
+++ run_train.sh
@@ -12,6 +12,6 @@ fi
 seeds=(1 2 3)
 # seeds=(1 2 3)
 for seed in "${seeds[@]}"; do
-  ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-AMPShortStairs-Unitree-Go2-v0 --headless --seed $seed --log_dir "${datetime}_amp_basic_v3slow_task0.75" --logger wandb --log_project_name amp_v3slow_task0.75_final --max_iterations 20000 --num_envs 4096
+  ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-AMPShortStairs-Unitree-Go2-v0 --headless --seed $seed --log_dir "${datetime}_amp_basic_v3slow_task0.25" --logger wandb --log_project_name amp_v3slow_task0.25_final --max_iterations 20000 --num_envs 4096
   # ./isaaclab.sh -p source/standalone/workflows/rsl_rl/train.py --task Isaac-Velocity-Stairs-ComplexReward-Unitree-Go2-v0 --headless --seed $seed --log_dir "${datetime}_complex" --logger wandb --log_project_name complex_v2 --max_iterations 10000 --num_envs 4096
 done
