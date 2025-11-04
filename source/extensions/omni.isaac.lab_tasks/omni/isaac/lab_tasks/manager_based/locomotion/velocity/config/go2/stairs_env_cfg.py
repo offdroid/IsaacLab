179544f93d49bf8d125cff41ffd6f6a79299c00d
Filip Skubacz <filip.skubacz@tum.de>
2025-11-04 09:36:12 +0100

    Fix wrongly enabled curriculum

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
@@ -384,7 +384,7 @@ class AMPUnitreeGo2StairsEnvCfg(LocomotionVelocityRoughEnvCfg):
 
         self.terrain_type = "stairs"
         parameters.set_terrain(self)
-        parameters.set_curriculum(self, enable=True)
+        parameters.set_curriculum(self, enable=False)
         parameters.set_stairs_env_cfg_cmds(self)
         parameters.set_stairs_env_cfg_reset_base(self)
         parameters.add_relative_position_on_stairs_observation(self)
