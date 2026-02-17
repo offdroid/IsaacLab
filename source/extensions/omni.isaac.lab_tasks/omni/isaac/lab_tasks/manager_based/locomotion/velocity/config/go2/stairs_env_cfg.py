afb3b02105289cffd956b83063ca316a0667ab23
Filip Skubacz <filip.skubacz@tum.de>
2026-02-17 13:07:28 +0100

    Add yaw to obs

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/stairs_env_cfg.py
@@ -284,7 +284,7 @@ class AMPUnitreeGo2StairsEnvCfg(LocomotionVelocityRoughEnvCfg):
         parameters.set_stairs_env_cfg_cmds(self)
         parameters.set_stairs_env_cfg_reset_base(self)
         parameters.add_relative_position_on_stairs_observation(self)
-        self.observations.policy.yaw = None
+        #self.observations.policy.yaw = None
         self.observations.policy.relative_position = None
         #parameters.add_stair_parameters_observation(self)
 
